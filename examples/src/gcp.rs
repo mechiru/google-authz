b3ff11f23f7b15926a5f057e1e73ac49f612a68a
mechiru <46036584+mechiru@users.noreply.github.com>
2021-05-08 23:34:47 +0900

    add examples

---
+++ examples/src/gcp.rs
@@ -0,0 +1,41 @@
+use googapis::{
+    google::pubsub::v1::{publisher_client::PublisherClient, ListTopicsRequest},
+    CERTIFICATES,
+};
+use google_authz::AddAuthorization;
+use tonic::{
+    transport::{Certificate, Channel, ClientTlsConfig},
+    Request,
+};
+
+use std::{env, error::Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+
+    let project = env::args().nth(1).expect("cargo run --example gcp -- <GCP_PROJECT_ID>");
+
+    let tls_config = ClientTlsConfig::new()
+        .ca_certificate(Certificate::from_pem(CERTIFICATES))
+        .domain_name("pubsub.googleapis.com");
+
+    let channel = Channel::from_static("https://pubsub.googleapis.com")
+        .tls_config(tls_config)?
+        .connect()
+        .await?;
+
+    let channel = AddAuthorization::init(channel).await;
+
+    let mut client = PublisherClient::new(channel);
+    let resp = client
+        .list_topics(Request::new(ListTopicsRequest {
+            project: format!("projects/{}", project),
+            page_size: 10,
+            ..Default::default()
+        }))
+        .await?;
+    println!("response = {:?}", resp);
+
+    Ok(())
+}
---
+++ examples/src/hyper.rs
@@ -0,0 +1,29 @@
+use google_authz::Client;
+use hyper::{body::to_bytes, Body, Uri};
+use hyper_rustls::HttpsConnector;
+
+use std::{convert::TryFrom, env, error::Error};
+
+// https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics/list
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+
+    let project = env::args().nth(1).expect("cargo run --bin hyper -- <GCP_PROJECT_ID>");
+
+    let https = HttpsConnector::with_native_roots();
+    let client = hyper::Client::builder().build::<_, Body>(https);
+    let mut client = Client::new(client).await;
+
+    let uri = Uri::try_from(format!(
+        "https://pubsub.googleapis.com/v1/projects/{}/topics?alt=json&prettyPrint=true",
+        project
+    ))?;
+    let (parts, body) = client.get(uri).await?.into_parts();
+    println!("response parts = {:?}", parts);
+
+    let body = String::from_utf8(to_bytes(body).await?.to_vec())?;
+    println!("resposne body = `{}`", body);
+
+    Ok(())
+}
